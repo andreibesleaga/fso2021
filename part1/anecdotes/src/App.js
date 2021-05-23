daab586d7f567caabbb413e98b4be566a3ae25a8
Andrei <33538982+andreibesleaga@users.noreply.github.com>
2021-05-23 17:45:28 +0300

    app anecdotes : exercises 1.12 - 1.14 - anecdotes step1-step3

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length));
+  const [selected, setSelected] = useState(0)
+
+  const setVoting = (selected) => {
+    const votes = [...points]
+    votes[selected] += 1
+    setPoints(votes)
+  }
+
+  const MostVotes = () => {
+    var max = Math.max(...points);
+    const votes = [...points]
+    const maxKey = parseInt(Object.entries(votes).find(([key, value]) => value === max ? key : null));
+    return (
+      <div>
+        <h1>Anecdote with most votes</h1>
+        {anecdotes[maxKey]} <br />
+        has {max} votes <br />
+      </div>
+    )  
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]} <br />
+      has {points[selected]} votes <br />
+      <br />
+      <Button handleClick={() => setVoting(selected)} text="vote" />
+      <Button handleClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} text="next anecdote" />
+      <MostVotes />
+    </div>
+  )
+}
+
+export default App
